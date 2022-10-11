fbbdacbb82990a73a45101add8e8afaaef03f579
Baris0 <barisseckin0@gmail.com>
2022-10-12 01:58:35 +0300

    cross origin bugfix

--- src/main/java/com/websocket/demo/controller/ChatController.java
+++ src/main/java/com/websocket/demo/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.websocket.demo.controller;
 
 import com.websocket.demo.model.WsMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
